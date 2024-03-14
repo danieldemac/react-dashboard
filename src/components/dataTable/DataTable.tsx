935147f9c0c5e1c9b7a2c6e85245cc795765b4e7
danieldemac <12dancabral@gmail.com>
2024-03-14 15:48:34 -0300

    ADições dia 14-03

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,61 +1,46 @@
 import { DataGrid, GridColDef, GridToolbar, GridValueGetterParams } from "@mui/x-data-grid";
 import "./dataTable.scss";
+import { Link } from "react-router-dom";
 
-const DataTable = () => {
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params: GridValueGetterParams) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
+const DataTable = (props:Props) => {
+
+    const handleDelete = (id:number)=>{
+      console.log(id + "foi deletado!")
+    }
+
+    const actionColumn:GridColDef = {
+      field: 'actions', headerName:'Actions', width:200,
+      renderCell:(params) => {
+        return <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+          <img src="/view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={()=>handleDelete(params.row.id)}>
+            <Link>
+            <img src="/delete.svg" alt="" />
+            </Link> 
+          </div>
+        
+        </div>
+      }
+    }
+    
+    type Props = {
+      columns: GridColDef[],
+      rows:object[]
+      slug:string;
+    }
 
     return (
         <div className="dataTable">
             <DataGrid
             className="dataGrid"
-            rows={rows}
-            columns={columns}
+            rows={props.rows}
+            columns={[...props.columns, actionColumn]}
             initialState={{
             pagination: {
                 paginationModel: {
-                pageSize: 5,
+                pageSize: 10,
                 },
             },
             }}
@@ -69,6 +54,9 @@ const DataTable = () => {
             pageSizeOptions={[5]}
             checkboxSelection
             disableRowSelectionOnClick
+            disableColumnFilter
+            disableDensitySelector
+            disableColumnSelector
              />
         </div>
     )
