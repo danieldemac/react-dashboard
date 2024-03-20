3f316f21527750259158c30905102ebf8595bde7
danieldemac <12dancabral@gmail.com>
2024-03-20 16:12:52 -0300

    Correção url

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -11,7 +11,7 @@ const Menu = () => {
                 <span className="title">{item.title}</span>
                 {item.listItems.map((listItem)=>(
                 
-                <Link to="/" className="listItem" key={listItem.id}>
+                <Link to={listItem.url} className="listItem" key={listItem.id}>
                     <img src={listItem.icon} alt="" />
                     <span className="listItemTitle">{listItem.title}</span>
                 </Link>
